9a45cfc737b329e788cdd9170521be388fb3e798
Arseniy <arsmatus@list.ru>
2023-03-23 23:19:46 +0300

    add images to events cell

--- EventPasser/Classes/Modules/Events/EventTableViewCell.swift
+++ EventPasser/Classes/Modules/Events/EventTableViewCell.swift
@@ -18,6 +18,7 @@ class EventTableViewCell: UITableViewCell {
         stackView.axis = .vertical
         stackView.alignment = .leading
         stackView.spacing = 3
+        stackView.distribution = .fillProportionally
 
         return stackView
     }()
@@ -28,26 +29,25 @@ class EventTableViewCell: UITableViewCell {
         return imageView
     }()
 
+    private lazy var eventImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 8
+        imageView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
+        return imageView
+    }()
+
     func setCell(with event: EventEntity, isSet: Bool = false) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .long
         dateFormatter.timeStyle = .short
-        self.name.text = event.wrappedTitle
-        self.address.text = event.address
-        self.timeStart.text = dateFormatter.string(from: event.wrappedTimeStart)
-        self.guestsCount.text = "\(event.wrappedCurrentAmountOfTickets)/\(event.wrappedMaxCount)"
+        name.text = event.wrappedTitle
+        address.text = event.address
+        timeStart.text = dateFormatter.string(from: event.wrappedTimeStart)
+        guestsCount.text = "\(event.wrappedCurrentAmountOfTickets)/\(event.wrappedMaxCount)"
+        eventImageView.image = event.convertedImage
 
-        var image: UIImage?
-        image = UIImage(named: "ticket.fill")?.withRenderingMode(.alwaysTemplate)
-        isSetImageView.image = image
-        let largeConfig = UIImage.SymbolConfiguration(textStyle: .title2)
-        image = image?.withConfiguration(largeConfig)
-
-        if isSet {
-            isSetImageView.tintColor = .systemGreen
-        } else {
-            isSetImageView.tintColor = .systemGray
-        }
+        setTicketImage(isSet: isSet)
     }
 
     var backView = UIView()
@@ -74,8 +74,10 @@ class EventTableViewCell: UITableViewCell {
         backView.backgroundColor = .customBackgroundColor
         backView.layer.cornerRadius = 8
 
+        eventImageView.translatesAutoresizingMaskIntoConstraints = false
         stackView.translatesAutoresizingMaskIntoConstraints = false
         isSetImageView.translatesAutoresizingMaskIntoConstraints = false
+        backView.addSubview(eventImageView)
         backView.addSubview(stackView)
         backView.addSubview(isSetImageView)
 
@@ -88,20 +90,28 @@ class EventTableViewCell: UITableViewCell {
         stackView.addArrangedSubview(timeStart)
         stackView.addArrangedSubview(guestsCount)
 
+        
         NSLayoutConstraint.activate([
-            isSetImageView.centerYAnchor.constraint(equalTo: backView.centerYAnchor),
-            isSetImageView.trailingAnchor.constraint(equalTo: backView.trailingAnchor, constant: -20),
+            eventImageView.centerYAnchor.constraint(equalTo: backView.centerYAnchor),
+            eventImageView.leadingAnchor.constraint(equalTo: backView.leadingAnchor),
+            eventImageView.topAnchor.constraint(equalTo: backView.topAnchor),
+            eventImageView.bottomAnchor.constraint(equalTo: backView.bottomAnchor),
+            eventImageView.widthAnchor.constraint(equalToConstant: 120),
+            eventImageView.heightAnchor.constraint(equalToConstant: 120),
 
             stackView.centerYAnchor.constraint(equalTo: backView.centerYAnchor),
-            stackView.leadingAnchor.constraint(equalTo: backView.leadingAnchor, constant: 30),
-            stackView.trailingAnchor.constraint(equalTo: isSetImageView.leadingAnchor),
+            stackView.leadingAnchor.constraint(equalTo: eventImageView.trailingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: backView.trailingAnchor, constant: -10),
             stackView.topAnchor.constraint(equalTo: backView.topAnchor, constant: 5),
             stackView.bottomAnchor.constraint(equalTo: backView.bottomAnchor, constant: -5),
+            
+            isSetImageView.centerYAnchor.constraint(equalTo: guestsCount.centerYAnchor),
+            isSetImageView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
         ])
-
+        
         selectionStyle = .none
     }
-    
+
     override func setHighlighted(_ highlighted: Bool, animated: Bool) {
         super.setHighlighted(highlighted, animated: animated)
         UIView.animate(withDuration: 0.5, delay: 0) {
@@ -118,5 +128,19 @@ class EventTableViewCell: UITableViewCell {
     @available(*, unavailable)
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setTicketImage(isSet: Bool) {
+        var image: UIImage?
+        image = UIImage(named: "ticket.fill")?.withRenderingMode(.alwaysTemplate)
+        isSetImageView.image = image
+        let largeConfig = UIImage.SymbolConfiguration(textStyle: .title2)
+        image = image?.withConfiguration(largeConfig)
+
+        if isSet {
+            isSetImageView.tintColor = .systemGreen
+        } else {
+            isSetImageView.tintColor = .systemGray
+        }
     }
 }
